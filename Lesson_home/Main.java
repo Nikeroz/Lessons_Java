89c7c4565869bf701799f46a6dc91b6774ea430f
Nikeroz <88400397+Nikeroz@users.noreply.github.com>
2021-09-22 22:29:31 +0300

    ДЗ 3 урок

---
+++ Lesson_home/Main.java
@@ -0,0 +1,68 @@
+package Lesson_home;
+
+import java.util.Arrays;
+public class Main {
+        public static void main(String[] args){
+            oneMassive();
+            twoMassive();
+            threeMassive();
+            fourMassive();
+            fiveMassive(6,1);
+        }
+
+        public static void oneMassive(){
+            int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+            for (int i = 0; i < arr.length; i++){
+                if(arr[i] > 0){
+                    arr[i] = arr[i] - 1;
+                }else {
+                    arr[i] = arr[i] + 1;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+
+        public static void twoMassive(){
+            int[] arr = new int[100];
+            for (int i = 0; i < arr.length; i++){
+                arr[i] = i + 1;
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+
+        public static void threeMassive(){
+            int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] < 6){
+                    arr[i] = arr[i] *= 2;
+                }else{
+
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+
+        public static void fourMassive(){
+            int[][] arr = new int[4][4];
+
+            for (int i = 0; i < 4; i++){
+                for (int j = 0; j < 4; j++){
+                    arr[0][0] = 1;
+                    arr[1][1] = 1;
+                    arr[2][2] = 1;
+                    arr[3][3] = 1;
+                    System.out.print(arr[i][j] + " ");
+                }System.out.println();
+            }
+        }
+
+        public static void fiveMassive(int len, int initialValue){
+            int[] arr = new int[len];
+            for (int i = 0; i < len; i++){
+                arr[i] = initialValue;
+                System.out.print("[" + i + "]" + " " + arr[i] + " ");
+            }
+
+        }
+
+    }
